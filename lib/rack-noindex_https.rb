cfb3d61e8e29462e050a1d7d6750dfb452c39cf0
Josh Moore <joshsmoore@gmail.com>
2013-07-30 11:16:51 +0800

    fixed to work with rails

--- lib/rack-noindex_https.rb
+++ lib/rack-noindex_https.rb
@@ -10,7 +10,9 @@ module Rack
       @status, @headers, @body = @app.call(env)
       
       if env['rack.url_scheme'] == 'https' && (@headers['Content-Type'] =~ /application\/html/ || @headers['Content-Type'] =~ /text\/html/)
-        @body = @body.collect {|fragement| fragement.gsub(%r{</head>}, '<meta name="robots" content="noindex"></head>') } 
+        new_body = []
+        @body.each {|fragement| new_body << fragement.gsub(%r{</head>}, '<meta name="robots" content="noindex"></head>') } 
+        @body = new_body
       end
 
       [@status, @headers, @body]
