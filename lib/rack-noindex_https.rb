8279abf0176dfc6fae128a94213bb77ac38ef2a4
Josh Moore <joshsmoore@gmail.com>
2013-07-30 09:59:16 +0800

    removed debug information

--- lib/rack-noindex_https.rb
+++ lib/rack-noindex_https.rb
@@ -7,15 +7,11 @@ module Rack
     end
 
     def call(env)
-      puts "got here"
       @status, @headers, @body = @app.call(env)
-      puts env.inspect
       
       if env['rack.url_scheme'] == 'https' && (@headers['Content-Type'] =~ /application\/html/ || @headers['Content-Type'] =~ /text\/html/)
-        @body = @body.collect {|fragement| puts "updating"; fragement.gsub(%r{</head>}, '<meta name="robots" content="noindex"></head>') } 
+        @body = @body.collect {|fragement| fragement.gsub(%r{</head>}, '<meta name="robots" content="noindex"></head>') } 
       end
-
-      puts 'finished'
 
       [@status, @headers, @body]
     end
